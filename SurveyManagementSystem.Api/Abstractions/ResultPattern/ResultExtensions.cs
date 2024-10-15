da3448352ea9dd5501081346143b6d9de33ba564
Ashraf <mm0019701>
2024-10-15 18:07:29 +0300

    Add Result Pattern

---
+++ SurveyManagementSystem.Api/Abstractions/ResultPattern/ResultExtensions.cs
@@ -0,0 +1,30 @@
+﻿using SurveyBasket.Abstractions;
+using System.Reflection;
+
+namespace SurveyManagementSystem.Api.Abstractions.ResultPattern;
+
+public static class ResultExtensions
+{
+    public static ObjectResult ToProblem(this Result result)
+    {
+        if (result.IsSuccess)
+            throw new InvalidOperationException("cannot convert success result to problem");
+
+        var problem=Results.Problem(statusCode:result.Error.StatusCode);
+        var problemDetails = problem.GetType().GetProperty(nameof(ProblemDetails))!.GetValue(problem) as ProblemDetails;
+
+
+        problemDetails!.Extensions = new Dictionary<string, object?>
+        {
+            {
+                "errors",new[]
+                {
+                    result.Error.Code,
+                    result.Error.Description
+                }
+            }
+        };
+
+        return new ObjectResult(problemDetails);
+    }
+}
